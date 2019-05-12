fa46717706f039a2d4f664fe89f94f8a9c791694
Kendon Bell <kmb56@berkeley.edu>
2019-05-13 11:35:23 +1200

    add more directions to nav

--- castervoice/lib/navigation.py
+++ castervoice/lib/navigation.py
@@ -19,6 +19,10 @@ DIRECTION_STANDARD = {
     "dunce [E]": "down",
     "lease [E]": "left",
     "Ross [E]": "right",
+    "hum": "home",
+    "end": "end",
+    "sun": "pageup",
+    "doon": "pagedown",
     "back": "left"
 }
 '''
