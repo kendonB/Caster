dbf157cb19591b393af93ad6979dcd0e59e52e00
Kendon Bell <kmbell56@gmail.com>
2019-06-25 10:22:47 +1200

    fix

--- castervoice/lib/ccr/core/nav.py
+++ castervoice/lib/ccr/core/nav.py
@@ -321,7 +321,7 @@ class Navigation(MergeRule):
     "(left | lease)": "left", "(right | ross)": "right", "(up | sauce)": "up",
     "(down | dunce)": "down", "page (down | dunce)": "pgdown", "page (up | sauce)": "pgup", "space": "space"}
     button_dictionary_10 = {}
-    button_dictionary_10.update(caster_alphabet)
+    button_dictionary_10.update(alphanumeric.caster_alphabet)
     button_dictionary_10.update(text_punc_dict)
     button_dictionary_1 = {"(hum | lease wally | latch)": "home", "(end | ross wally | ratch)": "end", "insert": "insert", "zero": "0",
     "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six":"6", "seven": "7", "eight": "8", "nine": "9"}
