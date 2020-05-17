88bf06500bdd51173cdb70cd6087d8f1dba911ec
Kendon Bell <kmb56@berkeley.edu>
2020-05-18 09:39:39 +1200

    setup

--- Install_Caster_Kaldi.bat
+++ Install_Caster_Kaldi.bat
@@ -3,8 +3,8 @@ set currentpath=%~dp0
 echo Installation path: %currentpath%
 
 echo Installing Caster Dependencies
-C:\Python38_64\python.exe -m pip install -r "%currentpath%requirements.txt"
-C:\Python38_64\python.exe -m pip install dragonfly2[kaldi]
+C:\Python27\python.exe -m pip install -r "%currentpath%requirements.txt"
+C:\Python27\python.exe -m pip install dragonfly2[kaldi]
 
 echo Remember: Manually install kaldi a model. 
 echo See Caster kaldi install instructions on ReadTheDocs.
