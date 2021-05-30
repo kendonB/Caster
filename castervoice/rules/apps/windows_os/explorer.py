3196008d6e531cf4e9fd448c3c310da599f5dd3d
Kenny Bell <kmbell56@gmail.com>
2021-05-31 09:01:58 +1200

    sc

--- castervoice/rules/apps/windows_os/explorer.py
+++ castervoice/rules/apps/windows_os/explorer.py
@@ -4,42 +4,35 @@ from castervoice.lib.actions import Key, Text
 
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.state.short import R
-from castervoice.lib import settings
 
-file_dialogue_wait = "40"
-if settings.settings(["miscellaneous", "file_dialogue_wait"]):
-    file_dialogue_wait = str(settings.SETTINGS["miscellaneous"]["file_dialogue_wait"])
 
-class ExplorerRule(MappingRule):
+class IERule(MappingRule):
     mapping = {
         "address bar":
-            R(Key("escape/" + file_dialogue_wait + ", c-l")),
+            R(Key("a-d")),
         "new folder":
-            R(Key("escape, c-l, tab/" + file_dialogue_wait + ":3, cs-n")),
+            R(Key("cs-n")),
         "new file":
-            R(Key("escape, a-h, w")),
+            R(Key("a-f, w, t")),
         "(show | file | folder) properties":
-            R(Key("escape, a-enter")),
-        "get up [<n>]":
-            R(Key("escape, a-up"))*Repeat(extra="n"),
-        "get back [<n>]": 
-            R(Key("escape, a-left"))*Repeat(extra="n"),
-        "get forward [<n>]": 
-            R(Key("escape, a-right"))*Repeat(extra="n"),
+            R(Key("a-enter")),
+        "get up":
+            R(Key("a-up")),
+        "get back":
+            R(Key("a-left")),
+        "get forward":
+            R(Key("a-right")),
         "search [<text>]":
-            R(Key("escape, c-l, tab/" + file_dialogue_wait + "") + Text("%(text)s")),
+            R(Key("a-d, tab:1") + Text("%(text)s")),
         "(navigation | nav | left) pane":
-            R(Key("escape/10, c-l, tab/" + file_dialogue_wait + ":2")),
-        "(center|file|files|folder) (list | pane)":
-            R(Key("escape/10, c-l, tab/" + file_dialogue_wait + ":3")),
+            R(Key("a-d, tab:2")),
+        "(center pane | (file | folder) (pane | list))":
+            R(Key("a-d, tab:3")),
             # for the sort command below,
             # once you've selected the relevant heading for sorting using the arrow keys, press enter
         "sort [headings]":
-            R(Key("escape/10, c-l, tab/" + file_dialogue_wait + ":3, tab:1")),
-    }
-    extras = [IntegerRefST("n", 1, 10), Dictation("text")]
-    defaults = {
-        "n": 1,
+            R(Key("a-d, tab:4")),
+
     }
     extras = [
         Dictation("text"),
@@ -49,4 +42,4 @@ class ExplorerRule(MappingRule):
 
 
 def get_rule():
-    return ExplorerRule, RuleDetails(name="explorer", executable="explorer")
+    return IERule, RuleDetails(name="explorer", executable="explorer")
