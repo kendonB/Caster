dfb15e2e5b23fff15c45db689744fd43a4f548d2
Kendon Bell <kenny.bell@scarlatti.co.nz>
2023-07-19 16:18:07 +1200

    edit run caster kaldi

--- Run_Caster_Kaldi.bat
+++ Run_Caster_Kaldi.bat
@@ -4,6 +4,7 @@ echo Running Kaldi from Dragonfly CLI
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-python -m dragonfly load _*.py --engine kaldi --no-recobs-messages --engine-options "model_dir=kaldi_model, vad_padding_end_ms=300"
+set CASTER_USER_DIR=C:\Users\KennyBell\Dropbox\caster_windows_user&& python -m dragonfly load _*.py --engine kaldi --no-recobs-messages --engine-options "model_dir=kaldi_model, vad_padding_end_ms=300"
+
 
 pause 1
